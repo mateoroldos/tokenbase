1e9bd38f2a8b1fc38dccf3e794d2f7f84e368a97
mateoroldos <mateoroldos19@gmail.com>
2023-04-16 16:10:03 -0300

    Create o7 App

---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1>You are using the o7 stack</h1>
---
+++ src/routes/api/trpc/[...trpc]/+server.ts
@@ -0,0 +1,16 @@
+import { appRouter } from '$lib/server/routes/_app';
+import { createContext } from '$lib/server/context';
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
+import type { RequestHandler } from './$types';
+
+const handler: RequestHandler = async (event) => {
+	return fetchRequestHandler({
+		endpoint: '/api/trpc',
+		req: event.request,
+		router: appRouter,
+		createContext,
+	});
+};
+
+export const GET = handler;
+export const POST = handler;
