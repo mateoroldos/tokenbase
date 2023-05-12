df2f2b25ee70b965f7d8a1f8c134857ec239c3a3
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-05-12 18:12:42 -0300

    DimensionToken component done

--- src/lib/features/token-ui/ui/token-types/DimensionToken.svelte
+++ src/lib/features/token-ui/ui/token-types/DimensionToken.svelte
@@ -1,7 +1,47 @@
 <script lang="ts">
-	import type { Token } from '$lib/features/token-groups-store/types/token-interface';
+	import type { Token } from '$lib/features/token-groups-store/types/token-interface'
 
-	export let token: Token<'dimension'>;
+	export let token: Token<'dimension'>
+
+	let dimensionValue: number
+	let dimensionUnit: 'px' | 'em' | 'rem'
+
+	let options = [
+		{
+			value: 'px',
+			text: 'px'
+		},
+		{
+			value: 'em',
+			text: 'em'
+		},
+		{
+			value: 'rem',
+			text: 'rem'
+		}
+	]
+
+	$: token.value = `${dimensionValue}${dimensionUnit}`
 </script>
 
-<div>{token.value}</div>
+<div class="flex gap-6">
+	<div>
+		<input
+			class="border border-solid border-black"
+			type="number"
+			bind:value={dimensionValue}
+		/>
+	</div>
+	<div>
+		<select
+			class="border border-solid border-black"
+			bind:value={dimensionUnit}
+		>
+			{#each options as option}
+				<option value={option.value}>
+					{option.text}
+				</option>
+			{/each}
+		</select>
+	</div>
+</div>
