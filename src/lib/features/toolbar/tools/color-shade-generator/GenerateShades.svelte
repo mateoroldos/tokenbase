be58d3858900ecbbe7d19f4f28b11273ba6dc5f6
mateoroldos <mateoroldos19@gmail.com>
2023-05-17 11:14:40 -0300

    feat: Add index to shade generator

---
+++ src/lib/features/toolbar/tools/color-shade-generator/GenerateShades.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+	import type { createTokensGroupStore } from '$lib/features/token-groups-store/tokensGroup'
+	import Icon from '@iconify/svelte'
+	import { getContext } from 'svelte'
+	import type { Writable } from 'svelte/store'
+	import { generateColorShades } from './GenerateShades'
+	import type {
+		IToken,
+		TokenType,
+		TokenValue
+	} from '$lib/features/token-groups-store/types/token-interface'
+
+	const designTokensGroupStore: ReturnType<typeof createTokensGroupStore> =
+		getContext('designTokensGroupStore')
+	const selectedTokens: Writable<IToken[]> = getContext('selectedTokens')
+
+	const handleGenerateShades = () => {
+		if (validShade) {
+			console.log('Generating color shades...')
+
+			const colorShades = generateColorShades(
+				$selectedTokens[0]?.value as TokenValue<'color'>,
+				$selectedTokens[1]?.value as TokenValue<'color'>,
+				10
+			)
+
+			const colorShadesTokens = colorShades.map((colorShade, i) => ({
+				type: 'color' as TokenType,
+				value: colorShade,
+				name: `${$selectedTokens[0]?.name}-shade-${i}`
+			}))
+
+			const firstSelectedTokenIndex = $designTokensGroupStore.findIndex(
+				(token) => token.name === $selectedTokens[0]?.name
+			)
+
+			const lastSelectedTokenIndex = $designTokensGroupStore.findIndex(
+				(token) => token.name === $selectedTokens[1]?.name
+			)
+
+			designTokensGroupStore.bulkAddTokens(
+				$page.params.groupId as string,
+				colorShadesTokens,
+				firstSelectedTokenIndex > lastSelectedTokenIndex
+					? lastSelectedTokenIndex - 1
+					: firstSelectedTokenIndex
+			)
+		}
+	}
+
+	$: validShade =
+		$selectedTokens.length === 2 &&
+		$selectedTokens[0]?.type === 'color' &&
+		$selectedTokens[1]?.type === 'color'
+</script>
+
+{#if validShade}
+	<button
+		class="flex flex-row items-center gap-2"
+		on:click={handleGenerateShades}
+	>
+		<Icon icon="tabler:color-swatch" />
+		Generate shades
+		{#each $selectedTokens as a}
+			{a}
+		{/each}
+	</button>
+{/if}
--- src/lib/features/toolbar/ui/Toolbar.svelte
+++ src/lib/features/toolbar/ui/Toolbar.svelte
@@ -1,57 +1,7 @@
 <script lang="ts">
-	import { page } from '$app/stores'
-	import type { createTokensGroupStore } from '$lib/features/token-groups-store/tokensGroup'
-	import Icon from '@iconify/svelte'
-	import { getContext } from 'svelte'
-	import type { Writable } from 'svelte/store'
-	import { generateColorShades } from '../tools/color-shade-generator/GenerateShades'
-	import type {
-		IToken,
-		TokenType,
-		TokenValue
-	} from '$lib/features/token-groups-store/types/token-interface'
-
-	const designTokensGroupStore: ReturnType<typeof createTokensGroupStore> =
-		getContext('designTokensGroupStore')
-	const selectedTokens: Writable<IToken[]> = getContext('selectedTokens')
-
-	const handleGenerateShades = () => {
-		if (validShade) {
-			console.log('Generating color shades...')
-
-			const colorShades = generateColorShades(
-				$selectedTokens[0]?.value as TokenValue<'color'>,
-				$selectedTokens[1]?.value as TokenValue<'color'>,
-				10
-			)
-
-			const colorShadesTokens = colorShades.map((colorShade) => ({
-				type: 'color' as TokenType,
-				value: colorShade
-			}))
-
-			designTokensGroupStore.bulkAddTokens(
-				$page.params.groupId as string,
-				colorShadesTokens
-			)
-		}
-	}
-
-	$: validShade =
-		$selectedTokens.length === 2 &&
-		$selectedTokens[0]?.type === 'color' &&
-		$selectedTokens[1]?.type === 'color'
+	import GenerateShades from '../tools/color-shade-generator/GenerateShades.svelte'
 </script>
 
-{#if validShade}
-	<button
-		class="flex flex-row items-center gap-2"
-		on:click={handleGenerateShades}
-	>
-		<Icon icon="tabler:color-swatch" />
-		Generate shades
-		{#each $selectedTokens as a}
-			{a}
-		{/each}
-	</button>
-{/if}
+<div>
+	<GenerateShades />
+</div>
