23fd5dda9ca12267f184d8f248deddb801422b2a
Juan Pedro Falco <falcojuanpedro@gmail.com>
2023-07-03 12:10:31 -0300

    Font Family validation suite

--- src/lib/features/token-ui/ui/token-types/FontFamilyToken.svelte
+++ src/lib/features/token-ui/ui/token-types/FontFamilyToken.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
 	import type { IToken } from '$lib/features/token-groups-store/types/token-interface'
+	import Icon from '@iconify/svelte'
 	import {
 		Tooltip,
 		TooltipContent,
 		TooltipProvider,
 		TooltipTrigger
 	} from '$lib/components/ui/tooltip'
-	import {
-		Accordion,
-		AccordionContent,
-		AccordionItem,
-		AccordionTrigger
-	} from '$components/ui/accordion'
-	import { Minus, Plus } from 'lucide-svelte'
+	import fontFamilySuite from '$lib/features/token-management/font-family/fontFamilySuite'
+	import InputWrapper from '$components/InputWrapper.svelte'
 
 	export let token: IToken<'fontFamily'>
 
 	$: isAlias = token.alias !== undefined
+
+	const handleChange = (input: Event) => {
+		if (isAlias) return
+		const target = input.target as HTMLInputElement
+		const name = target.name
+
+		res = fontFamilySuite(target.value, name)
+	}
+
+	let res = fontFamilySuite.get()
 
 	function addInput() {
 		token.value = [...token.value, '']
@@ -28,12 +34,12 @@
 	}
 </script>
 
-<div class="flex flex-row gap-4">
+<div class="flex flex-row gap-2">
 	<TooltipProvider>
 		<Tooltip>
 			<TooltipTrigger>
-				<button class="ml-3" on:click={addInput}
-					><Plus class="w-4 items-center" /></button
+				<button class="mr-2" on:click={addInput}
+					><Icon icon="tabler:square-rounded-plus" /></button
 				>
 			</TooltipTrigger>
 			<TooltipContent>
@@ -42,44 +48,36 @@
 		</Tooltip>
 	</TooltipProvider>
 	<div class="flex flex-col gap-4">
-		<Accordion type="single" collapsible>
-			<AccordionItem value="item-1">
-				<AccordionTrigger
-					><input
-						class="mr-2 w-52 rounded-md border-2 border-solid border-gray-200 px-2 py-1 text-xs"
-						type="text"
-						placeholder="Your fonts"
-						disabled
-					/></AccordionTrigger
+		{#each token.value as value, i}
+			<div class="flex">
+				<InputWrapper
+					name="fontFamilyToken"
+					errors={res.getErrors('fontFamilyToken')}
+					isValid={res.isValid('fontFamilyToken')}
 				>
-				{#each token.value as value, i}
-					<AccordionContent>
-						<div class="flex">
-							<input
-								class="mr-2 w-52 rounded-md border-2 border-solid border-gray-200 px-2 py-1"
-								type="text"
-								bind:value
-								{...isAlias ? { disabled: true } : {}}
-							/>
-							<TooltipProvider>
-								<Tooltip>
-									<TooltipTrigger>
-										<button
-											class="ml-2 self-center"
-											on:click={() => removeInput(i)}
-										>
-											<Minus class="w-4" /></button
-										>
-									</TooltipTrigger>
-									<TooltipContent>
-										<p>Remove Font Family</p>
-									</TooltipContent>
-								</Tooltip>
-							</TooltipProvider>
-						</div>
-					</AccordionContent>
-				{/each}
-			</AccordionItem>
-		</Accordion>
+					<input
+						name="fontFamilyToken"
+						class="mr-2 w-52 rounded-md border-2 border-solid border-gray-200 px-2 py-1"
+						type="text"
+						bind:value
+						on:input={handleChange}
+						{...isAlias ? { disabled: true } : {}}
+					/>
+				</InputWrapper>
+
+				<TooltipProvider>
+					<Tooltip>
+						<TooltipTrigger>
+							<button class="ml-2 self-center" on:click={() => removeInput(i)}>
+								<Icon icon="tabler:square-rounded-x" /></button
+							>
+						</TooltipTrigger>
+						<TooltipContent>
+							<p>Remove Font Family</p>
+						</TooltipContent>
+					</Tooltip>
+				</TooltipProvider>
+			</div>
+		{/each}
 	</div>
 </div>
