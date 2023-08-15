78a7796a924df430da808736b5852812511c72c0
Juan Pedro Falco <falcojuanpedro@gmail.com>
2023-08-15 16:36:24 -0300

    Design options for home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,7 +43,9 @@
 	}
 </script>
 
-<section class="flex flex-col items-start justify-center gap-8 p-20">
+<section
+	class="flex min-h-screen flex-col items-center justify-center gap-4 p-20"
+>
 	<h2 class="text-2xl font-medium">Welcome to Tokenbase</h2>
 	<div class="flex flex-row flex-wrap justify-between gap-7">
 		{#each $tokenbaseMainStore as designSystem}
@@ -61,27 +63,45 @@
 			</a>
 		{/each}
 	</div>
-	<Dialog>
-		<DialogTrigger class={buttonVariants({ variant: 'default' })}>
-			<Plus class="mr-2 h-4 w-4" />
-			Add Design System
-		</DialogTrigger>
-		<DialogContent class="max-w-sm">
-			<DialogHeader>
-				<DialogTitle>Add new Design System</DialogTitle>
-			</DialogHeader>
-			<div class="grid gap-4 py-4">
-				<div class="flex flex-col gap-2">
-					<Label>Design System name</Label>
-					<Input id="name" bind:value={desingSystemName} class="col-span-3" />
+	{#if $tokenbaseMainStore.length === 0}
+		<div class="relative flex w-3/6 flex-row gap-2">
+			<Input
+				id="name"
+				bind:value={desingSystemName}
+				class="bg-red col-span-5 w-full pr-12 drop-shadow-sm placeholder:text-slate-400"
+				placeholder="Write the name of your first design system"
+			/>
+			<Button
+				on:click={handleAddDesignSystem}
+				class="bg-tertiary absolute right-0 top-0 flex h-full items-center px-2 font-normal text-slate-400 hover:bg-transparent"
+			>
+				<Plus class="mr-1 h-4" />
+				Create
+			</Button>
+		</div>
+	{:else}
+		<Dialog>
+			<DialogTrigger class={buttonVariants({ variant: 'default' })}>
+				<Plus class="mr-2 h-4 w-4" />
+				Add Design System
+			</DialogTrigger>
+			<DialogContent class="max-w-sm">
+				<DialogHeader>
+					<DialogTitle>Add new Design System</DialogTitle>
+				</DialogHeader>
+				<div class="grid gap-4 py-4">
+					<div class="flex flex-col gap-2">
+						<Label>Design System name</Label>
+						<Input id="name" bind:value={desingSystemName} class="col-span-3" />
+					</div>
 				</div>
-			</div>
-			<DialogFooter>
-				<Button on:click={handleAddDesignSystem}>
-					<Plus class="mr-2 h-4 w-4" />
-					Create
-				</Button>
-			</DialogFooter>
-		</DialogContent>
-	</Dialog>
+				<DialogFooter>
+					<Button on:click={handleAddDesignSystem}>
+						<Plus class="mr-2 h-4 w-4" />
+						Create
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	{/if}
 </section>
