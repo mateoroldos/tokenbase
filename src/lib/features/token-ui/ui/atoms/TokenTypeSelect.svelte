324b36b36348e923cdc935db314948777882a0fc
Juan Pedro Falco <falcojuanpedro@gmail.com>
2023-08-28 13:32:32 -0300

    Clean code

--- src/lib/features/token-ui/ui/atoms/TokenTypeSelect.svelte
+++ src/lib/features/token-ui/ui/atoms/TokenTypeSelect.svelte
@@ -21,15 +21,3 @@
 	</Select.Content>
 	<Select.Input />
 </Select.Root>
-
-<!-- <select
-	bind:value
-	on:change
-	class="appearance-none rounded-sm border-none bg-gray-200 px-2 py-1 text-xs font-medium leading-tight text-gray-700 focus:outline-none"
->
-	{#each tokenTypesArray as contentType}
-		<option value={contentType}>
-			{contentType}
-		</option>
-	{/each}
-</select> -->
