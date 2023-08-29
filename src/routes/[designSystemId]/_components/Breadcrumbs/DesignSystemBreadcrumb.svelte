2e0289be52b6bcf1fcbe149f196711ce6975d211
Juan Pedro Falco <falcojuanpedro@gmail.com>
2023-08-29 17:27:25 -0300

    Search bar and select for design systems

--- src/routes/[designSystemId]/_components/Breadcrumbs/DesignSystemBreadcrumb.svelte
+++ src/routes/[designSystemId]/_components/Breadcrumbs/DesignSystemBreadcrumb.svelte
@@ -4,6 +4,9 @@
 	import { getContext } from 'svelte'
 	import type { createDesignSystemsStore } from '$lib/features/token-groups-store/designSystemsIds'
 	import type { createGroupsStore } from '$lib/features/token-groups-store/groups'
+	import * as Select from '$lib/components/ui/select'
+	import { Input } from '$lib/components/ui/input'
+	import { Search } from 'lucide-svelte'
 
 	const designTokensGroupStore: ReturnType<typeof createGroupsStore> =
 		getContext('designTokensGroupStore')
@@ -30,30 +33,52 @@
 		}
 	}
 
-	const handleChangeDesignSystem = (event: Event) => {
-		const select = event.target as HTMLSelectElement
-		const designSystemId = select.value
-
+	const handleChangeDesignSystem = (e: Event) => {
+		let designSystemId = (e.target?.dataset.value).replace(/"/g, '')
 		goto(`/${designSystemId}`)
 	}
+	let filter = ''
 </script>
 
 <div class="flex flex-row items-center">
-	<a
-		href={`/${$page.params.designSystemId}`}
-		class="text-md text-gray-500 hover:underline"
-	>
-		{$tokenBaseMainStore[activeDesignSystemIndex]?.name}
-	</a>
-
-	<!-- TODO: replace with select component -->
-	<!-- <select
-		name="design-system"
-		id="design-system"
-		on:change={handleChangeDesignSystem}
-	>
-		{#each $tokenBaseMainStore.designSystems as designSystem}
-			<option value={designSystem.id}>{designSystem.name}</option>
-		{/each}
-	</select> -->
+	<Select.Root>
+		<Select.Trigger class="focus:ring-3 w-[180px] border-none">
+			<Select.Value
+				placeholder={$tokenBaseMainStore[activeDesignSystemIndex]?.name}
+			/>
+		</Select.Trigger>
+		<Select.Content>
+			<Select.Group>
+				<div class="flex flex-row">
+					<Search
+						class=" 
+					 ml-3 mr-0 flex h-4 w-4  self-center "
+						color="gray"
+					/>
+					<Input
+						class="focus:ring-3 focus-visible:ring-3 border-none "
+						value={filter}
+						placeholder="Search system..."
+						on:input={(e) => (filter = e.target.value)}
+					/>
+				</div>
+				<Select.Separator />
+				<Select.Label class="pl-6">Design Systems</Select.Label>
+				{#each $tokenBaseMainStore as designSystem}
+					{#if designSystem.name.toLowerCase().includes(filter.toLowerCase())}
+						<Select.Item
+							class="pl-6"
+							on:m-click={(e) => {
+								handleChangeDesignSystem(e)
+							}}
+							value={designSystem.id}
+						>
+							{designSystem.name}
+						</Select.Item>
+					{/if}
+				{/each}
+			</Select.Group>
+		</Select.Content>
+		<Select.Input name="favoriteFruit" />
+	</Select.Root>
 </div>
