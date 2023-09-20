e41b8e314c173387895cc603fcee7d3e479c0eaa
mateoroldos <mateoroldos19@gmail.com>
2023-09-20 14:12:55 -0300

    fix: Fix Table import

--- src/lib/components/ui/table/index.ts
+++ src/lib/components/ui/table/index.ts
@@ -1,11 +1,12 @@
-import Root from "./table.svelte";
-import Body from "./table-body.svelte";
-import Caption from "./table-caption.svelte";
-import Cell from "./table-cell.svelte";
-import Footer from "./table-footer.svelte";
-import Head from "./table-head.svelte";
-import Header from "./table-header.svelte";
-import Row from "./table-row.svelte";
+
+import Root from './Table.svelte'
+import Body from './table-body.svelte'
+import Caption from './table-caption.svelte'
+import Cell from './table-cell.svelte'
+import Footer from './table-footer.svelte'
+import Head from './table-head.svelte'
+import Header from './table-header.svelte'
+import Row from './table-row.svelte'
 
 export {
 	Root,
@@ -25,4 +26,4 @@ export {
 	Head as TableHead,
 	Header as TableHeader,
 	Row as TableRow
-};
+}
