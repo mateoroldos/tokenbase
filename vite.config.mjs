dc827ad947784b2cc38ee92139d1b13b9b410a7b
mateoroldos <mateoroldos19@gmail.com>
2023-09-20 14:50:37 -0300

    fix: Add node polyfills

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 
 /** @type {import('vite').UserConfig} */
 const config = {
@@ -10,6 +11,10 @@ const config = {
 			protocolImports: true
 		})
 	],
+	build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
+    },
 	resolve: {
 		alias: {
 			fs: 'memfs'
