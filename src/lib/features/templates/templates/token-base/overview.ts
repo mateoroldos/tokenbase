bfbde5f17f9a4797dae71b4235d7a62334f917d7
Juan Pedro Falco <falcojuanpedro@gmail.com>
2023-10-03 11:12:18 -0300

    Token-base basic template

---
+++ src/lib/features/templates/templates/token-base/overview.ts
@@ -0,0 +1,9 @@
+import type { Template } from '../../types/template-interface'
+
+export const overview: Template = {
+	name: 'Token base',
+	description: 'Tokenbase project template.',
+	path: '/src/lib/features/templates/templates/token-base/template.json',
+	tags: ['tailwind'],
+	type: 'design-system'
+}
