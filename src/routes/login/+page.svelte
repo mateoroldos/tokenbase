f0c58a0c2a7c7bfc7a7beb2f70ae0111a7aed43c
Juan Pedro Falco <falcojuanpedro@gmail.com>
2023-11-07 12:39:27 -0300

    Login and register pages design updates

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,80 +1,119 @@
 <script>
 	import { enhance } from '$app/forms'
+	import * as Card from '$lib/components/ui/card'
 	import { AlertCircle } from 'lucide-svelte'
+	import { Input } from '$lib/components/ui/input'
+	import Button from '$lib/components/ui/button/Button.svelte'
+	import HeroSection from '$lib/components/login/HeroSection.svelte'
 
 	export let form
 </script>
 
-<form
-	class="bg-base-200 mx-auto mt-20 flex max-w-sm flex-col items-center justify-center rounded-lg py-6"
-	use:enhance
-	action="?/login"
-	method="post"
->
-	<div class="form-control w-full max-w-xs">
-		<!-- svelte-ignore a11y-label-has-associated-control -->
-		<label class="label">
-			<span class="label-text">Nombre de usuario</span>
-		</label>
-		<input
-			type="text"
-			placeholder="Username"
-			class="input input-bordered w-full max-w-xs"
-			name="username"
-			required
-		/>
-		{#if form?.incorrect && form?.errors.usernameError !== null}
-			<div class="flex flex-row items-center text-red-600 bg-blend-color-burn">
-				<AlertCircle class="mr-2 h-4 w-4" />
-				<p class=" text-red-600">
-					{form?.errors.usernameError[0]}
-				</p>
-			</div>
-		{/if}
-	</div>
+<section class="flex flex-1 items-center justify-center bg-slate-50 p-20">
+	<div class="grid grid-cols-3 gap-10">
+		<div
+			class="max-w-l col col-span-1 flex flex-col items-center justify-center"
+		>
+			<Card.Root
+				class=" shadow-m flex w-full flex-1 flex-col justify-center border-slate-200 p-4 "
+			>
+				<Card.Header>
+					<img
+						src="/LOGO_TOKENBASE.png"
+						alt="Logo"
+						class="mb-6 h-8 w-8 object-cover"
+					/>
+					<Card.Title class=" text-2xl ">Log in</Card.Title>
+				</Card.Header>
+				<Card.Content class="text-sm">
+					<form
+						class="bg-base-200 flex flex-col rounded-lg"
+						use:enhance
+						action="?/login"
+						method="post"
+					>
+						<div class="form-control flex w-full max-w-xs flex-col gap-2 pt-3">
+							<!-- svelte-ignore a11y-label-has-associated-control -->
+							<label class="label">
+								<span class="label-text">Username</span>
+							</label>
+							<Input
+								type="text"
+								placeholder="Username"
+								class="input input-bordered w-full max-w-xs"
+								name="username"
+								required
+							/>
+							{#if form?.incorrect && form?.errors.usernameError !== null}
+								<div
+									class="flex flex-row items-center text-red-600 bg-blend-color-burn"
+								>
+									<AlertCircle class="mr-2 h-4 w-4" />
+									<p class=" text-red-600">
+										{form?.errors.usernameError[0]}
+									</p>
+								</div>
+							{/if}
+						</div>
 
-	<div class="form-control w-full max-w-xs">
-		<!-- svelte-ignore a11y-label-has-associated-control -->
-		<label class="label">
-			<span class="label-text">Contraseña</span>
-		</label>
-		<input
-			type="text"
-			placeholder="Password"
-			class="input input-bordered w-full max-w-xs"
-			name="password"
-			required
-		/>
-		{#if form?.incorrect && form?.errors.passwordError !== null}
-			<div class="flex flex-row items-center text-red-600 bg-blend-color-burn">
-				<AlertCircle class="mr-2 h-4 w-4" />
-				<p class="text-red-600">
-					{form?.errors.passwordError[0]}
-				</p>
-			</div>
-		{/if}
-	</div>
-	{#if form?.usernameError}
-		<div class="flex flex-row items-center text-red-600 bg-blend-color-burn">
-			<AlertCircle class="mr-2 h-4 w-4" />
-			<p class=" text-red-600">Username doesn't exist</p>
+						<div
+							class="form-control flex w-full max-w-xs flex-col gap-2 pb-3 pt-3"
+						>
+							<!-- svelte-ignore a11y-label-has-associated-control -->
+							<label class="label">
+								<span class="label-text">Password</span>
+							</label>
+							<Input
+								type="text"
+								placeholder="Password"
+								class="input input-bordered w-full max-w-xs"
+								name="password"
+								required
+							/>
+							{#if form?.incorrect && form?.errors.passwordError !== null}
+								<div
+									class="flex flex-row items-center text-red-600 bg-blend-color-burn"
+								>
+									<AlertCircle class="mr-2 h-4 w-4" />
+									<p class="text-red-600">
+										{form?.errors.passwordError[0]}
+									</p>
+								</div>
+							{/if}
+						</div>
+						{#if form?.usernameError}
+							<div
+								class="flex flex-row items-center text-red-600 bg-blend-color-burn"
+							>
+								<AlertCircle class="mr-2 h-4 w-4" />
+								<p class=" text-red-600">Username doesn't exist</p>
+							</div>
+						{/if}
+						{#if form?.passwordError}
+							<div
+								class="flex flex-row items-center text-red-600 bg-blend-color-burn"
+							>
+								<AlertCircle class="mr-2 h-4 w-4" />
+								<p class=" text-red-600">Wrong password</p>
+							</div>
+						{/if}
+
+						<Button class="btn btn-primary mt-4 max-w-xs" type="submit"
+							>Log in</Button
+						>
+
+						<p class="pt-4">
+							Dont have an account? <a
+								href="/register"
+								class="link link-hover font-semibold">Register</a
+							>.
+						</p>
+					</form>
+				</Card.Content>
+			</Card.Root>
 		</div>
-	{/if}
-	{#if form?.passwordError}
-		<div class="flex flex-row items-center text-red-600 bg-blend-color-burn">
-			<AlertCircle class="mr-2 h-4 w-4" />
-			<p class=" text-red-600">Wrong password</p>
+		<div class="col-span-2 flex items-center justify-center bg-slate-50">
+			<HeroSection />
 		</div>
-	{/if}
-
-	<button class="btn btn-primary mt-4 max-w-xs" type="submit"
-		>Iniciar Sesion</button
-	>
-
-	<p class="pt-4">
-		Dont have an account? <a
-			href="/register"
-			class="link link-hover font-semibold">Register</a
-		>.
-	</p>
-</form>
+	</div>
+</section>
