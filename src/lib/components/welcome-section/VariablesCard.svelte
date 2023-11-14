5153d728cb0b282ffda135b2a571b5fde8fef5a5
Juan Pedro Falco <falcojuanpedro@gmail.com>
2023-11-14 16:23:22 -0300

    Welcome section components reordered

---
+++ src/lib/components/welcome-section/VariablesCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import VariableCard from './VariableCard.svelte'
+	import * as Card from '$lib/components/ui/card'
+	import type { Variable } from '$lib/interface/interface.ts'
+
+	export let variables: Variable[]
+</script>
+
+<Card.Root class="border-none bg-slate-900 p-6 shadow-none">
+	<p class="pb-3 font-mono text-sm font-medium text-slate-50">
+		styles/variables.css
+	</p>
+	<div class="flex flex-col items-center gap-2">
+		{#each variables as variable}
+			<VariableCard {variable} variables={true} />
+		{/each}
+	</div>
+</Card.Root>
