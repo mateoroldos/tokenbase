1bba65555100cd045d79cebca8eaa2cfd535a752
mateoroldos <mateoroldos19@gmail.com>
2023-11-23 12:13:01 -0300

    feat: Try different query

--- src/lib/features/user-management/user/getStoredUserByEmail.ts
+++ src/lib/features/user-management/user/getStoredUserByEmail.ts
@@ -8,10 +8,7 @@ export const getStoredUserByEmail = async (email: string) => {
 	console.log('db', db)
 
 	console.log('maaaail', user)
-	console.log(
-		'la llamada',
-		await db.select().from(user).where(eq(user.email, email))
-	)
+	console.log('la llamada', await db.select().from(user))
 
 	const [storedUser] = await db.select().from(user).where(eq(user.email, email))
 	console.log('storedUser', storedUser)
