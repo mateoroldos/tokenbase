b7a51c5c3b3c8520f07ca113129fd39e71b9a3a8
mateoroldos <mateoroldos19@gmail.com>
2023-11-23 12:28:06 -0300

    feat: Try new way to connecting to Planetscale

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,14 +1,39 @@
 import { connect } from '@planetscale/database'
 import { drizzle } from 'drizzle-orm/planetscale-serverless'
 import * as schema from '$lib/db/schema'
-import { SECRET_DATABASE_URL } from '$env/static/private'
+import {
+	SECRET_DATABASE_HOST,
+	SECRET_DATABASE_USERNAME,
+	SECRET_DATABASE_PASSWORD
+} from '$env/static/private'
+import { user } from '$lib/db/schema'
 
-console.log('SECRET_DATABASE_URL', SECRET_DATABASE_URL)
+console.log('SECRET_DATABASE_URL', SECRET_DATABASE_HOST)
 
-export const connection = connect({
-	url: SECRET_DATABASE_URL
-})
+// export async function onRequestPost(context) {
+const pscaleConfig = {
+	host: SECRET_DATABASE_HOST,
+	username: SECRET_DATABASE_USERNAME,
+	password: SECRET_DATABASE_PASSWORD,
+	// add the following 3 lines for @planetscale/database >= 1.7.0
+	fetch: (url, init) => {
+		delete init['cache']
+		return fetch(url, init)
+	}
+}
+
+const connection = connect(pscaleConfig)
 
 export const db = drizzle(connection, { schema })
 
-console.log('drizzle DB!!', db)
+const query = await db.select().from(user)
+console.log(query)
+// }
+
+// export const connection = connect(pscaleConfig)
+
+// export const connection = connect({
+// 	url: SECRET_DATABASE_URL
+// })
+
+// export const db = drizzle(connection, { schema })
