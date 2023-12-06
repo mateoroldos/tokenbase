934f16bb39e236d1c128b861e83922fa80a1c63e
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-12-06 17:01:36 -0300

    feat: files enabling Github OAuth

---
+++ src/lib/features/user-management/components/forms/GithubOAuthBtn.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/Button.svelte'
+	import { Github } from 'lucide-svelte'
+
+	const handleGithubLogin = async () => {
+		const github = await fetch(`/api/github`).then((res) => res.json())
+
+		window.location.href = github
+	}
+</script>
+
+<Button on:click={handleGithubLogin} class="btn btn-primary mt-4 max-w-xs">
+	<Github class="h-4 w-4 mr-2" />
+	Continue with Github
+</Button>
