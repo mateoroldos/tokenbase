a611196fd90b57037391c79d9a95aeb5130f0a43
mateoroldos <mateoroldos19@gmail.com>
2023-12-12 12:02:18 -0300

    feat: Improve validation messages

--- src/lib/components/ui/form/form-validation.svelte
+++ src/lib/components/ui/form/form-validation.svelte
@@ -1,14 +1,31 @@
 <script lang="ts">
-	import { Form as FormPrimitive } from 'formsnap'
+	import { fly } from 'svelte/transition'
 	import { cn } from '$lib/utils'
-	import type { HTMLAttributes } from 'svelte/elements'
+	import { getFormField, type ValidationProps } from 'formsnap'
+	import { X } from 'lucide-svelte'
 
-	type $$Props = HTMLAttributes<HTMLParagraphElement>
-	let className: string | undefined | null = undefined
-	export { className as class }
+	type $$Props = ValidationProps
+	export let tag = 'div'
+	const { actions, errors, ids } = getFormField()
+	$: attrs = {
+		'data-fs-validation': '',
+		'data-fs-error': $errors ? '' : undefined,
+		id: $ids.validation
+	}
 </script>
 
-<FormPrimitive.Validation
-	class={cn('text-xs font-medium text-destructive', className)}
-	{...$$restProps}
-/>
+<svelte:element this={tag} use:actions.validation {...attrs} {...$$restProps}
+	>{#if $errors}
+		{#each $errors as error (error)}
+			<p
+				in:fly={{ y: -10, duration: 200 }}
+				class={cn(
+					'text-xs font-medium text-destructive flex flex-row items-center gap-[0.1rem]'
+				)}
+			>
+				<X class="w-3 h-3" />
+				{error}
+			</p>
+		{/each}
+	{/if}
+</svelte:element>
