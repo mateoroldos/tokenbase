173a0aa6d3b06e83f3b7e6bb72ab00c1a68e76d9
mateoroldos <mateoroldos19@gmail.com>
2023-12-12 18:46:59 -0300

    feat: Add logs to getGithubEmails

--- src/lib/services/github/github.ts
+++ src/lib/services/github/github.ts
@@ -1,12 +1,20 @@
 export const getGithubUserEmails = async (
 	token: string
 ): Promise<GitHubUserEmail[]> => {
+	console.log('getGithubUserEmails', token)
+
 	const response = await fetch('https://api.github.com/user/emails', {
 		headers: {
 			Authorization: `token ${token}`
 		}
 	})
+
+	console.log('response', response)
+
 	const emails: GitHubUserEmail[] = await response.json()
+
+	console.log('emails', emails)
+
 	return emails
 }
 
