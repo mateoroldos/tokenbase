865cd71cad0f9d1a531b8d3caa3f9493a98f0b05
mateoroldos <mateoroldos19@gmail.com>
2023-12-14 18:19:22 -0300

    feat: Make CategoryScrollBar conditional

--- src/routes/(marketing)/_components/Category/CategoryScrollBar.svelte
+++ src/routes/(marketing)/_components/Category/CategoryScrollBar.svelte
@@ -7,13 +7,15 @@
 	posts = posts.slice(0, Math.min(posts.length, 8))
 </script>
 
-<div class="flex overflow-x-scroll pb-10 hide-scroll-bar">
-	<div class="flex flex-wrap gap-6">
-		{#each posts as post (post.slug)}
-			<ArticleCard {post} />
-		{/each}
+{#if posts.length > 0}
+	<div class="flex overflow-x-scroll pb-10 hide-scroll-bar">
+		<div class="flex flex-wrap gap-6">
+			{#each posts as post (post.slug)}
+				<ArticleCard {post} />
+			{/each}
+		</div>
 	</div>
-</div>
+{/if}
 
 <style>
 	.hide-scroll-bar {
