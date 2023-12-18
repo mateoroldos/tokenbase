a314ad2aa2709ba7617ddd74aad29962da53a9fe
mateoroldos <mateoroldos19@gmail.com>
2023-12-18 15:48:43 -0300

    fix: Make posta not prerenderable

--- src/routes/api/blog/posts.json/+server.ts
+++ src/routes/api/blog/posts.json/+server.ts
@@ -1,10 +1,7 @@
 import { json } from '@sveltejs/kit'
-import { postsPerPage } from '../../../../lib/config'
 import fetchPosts from '../../../(marketing)/_assets/ts/fetchPosts'
 
-export const prerender = true
-
-export const GET = async ({ url }) => {
+export const GET = async () => {
 	const { posts } = await fetchPosts()
 	return json(posts)
 }
